de8b4e0b74611a41bcafbc4e809d66143f107fe0
Astoryed <astoryed@gmail.com>
2019-10-10 03:26:07 +0500

    Remove .git folder form client folder which was hidden

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+      <Provider store={store}>
+          <div className="App">
+              <AppNavBar/>
+
+              <Container>
+                  <ItemModal/>
+              </Container>
+
+
+              <ShoppingList/>
+          </div>
+      </Provider>
+  );
+}
+
+export default App;
---
+++ client/src/actions/types.js
@@ -0,0 +1,6 @@
+export const GET_ITEMS = 'GET_ITEMS';
+export const ADD_ITEM = 'ADD_ITEM';
+export const DELETE_ITEMS = 'DELETE_ITEMS';
+export const ITEMS_LOADING = 'ITEMS_LOADING';
+
+
